1109a458b0dc201939b006ea175c38ee1ea30648
Kuldeep-Chotrani <49199229+Kuldeep-Chotrani@users.noreply.github.com>
2021-01-27 15:12:31 +0530

    Adding files to GitHub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {Component} from 'react';
+import './App.css';
+import { CardList } from "./component/card-list/card-list.component";
+
+import { Search } from './component/search-component/search.component';
+class App extends Component
+{
+  constructor()
+  {
+    super();
+    this.state = {
+          monsters: [],
+          searchInput: ''
+          
+    };
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(response =>response.json())
+    .then(users=>this.setState({monsters: users}));
+  }
+  render()
+  {
+    const {monsters, searchInput} = this.state;
+    const filteredMonsters = monsters.filter(
+      monster => monster.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    return(
+      <div className="App">
+      <div ><h1 style = {{fontSize : 72, fontFamily: 'Bigelow Rules',   color: '#0ccac4'}}>Monsters Rolodex</h1></div>
+       <Search 
+         placeholder ='Enter monster'
+         handleChange = {e => this.setState({searchInput: e.target.value}
+       )}
+       />
+    
+      <CardList monsters = {filteredMonsters}>
+      </CardList>
     </div>
-  );
+    );
+  
 }
+}
+  
 
 export default App;
